786269ce837a2fbd8facf1b5fe149457327e26b8
= <=>
2023-11-04 17:33:28 +0530

    finally done

---
+++ answer/Raisedto1.exe.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <cmath>
+int main(int argc, char *argv[]){
+float a;
+float b;
+float result;
+a = 4.5;
+b = 2.5;
+result = pow(a,b);
+std::cout<<result;
+return 0;
+}
